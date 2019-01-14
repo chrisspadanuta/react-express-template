d5954ec62ef39acf7bda726eefde02dc16a75f13
Chris Spadanuta <chris.spadanuta@gmail.com>
2019-01-13 19:20:27 -0500

    got it working

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -2,24 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './routes/home';
-import Admin from './routes/admin';
-import Poll from './routes/poll';
-import Results from './routes/results';
-import buildPageWithStatus from './components/status-page-template';
 
 import './styles.css';
 
 function App() {
-  const AdminPageWithStatus = buildPageWithStatus(Admin, 'Poll Administration');
-  const PollPageWithStatus = buildPageWithStatus(Poll, 'Please answer some questions');
-
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/admin" component={AdminPageWithStatus} />
-        <Route exact path="/poll" component={PollPageWithStatus} />
-        <Route exact path="/results" component={Results} />
       </Switch>
     </BrowserRouter>
   );
--- frontend/routes/home.jsx
+++ frontend/routes/home.jsx
@@ -1,17 +1,175 @@
 import React from 'react';
-//import { add, subtract, createObject } from '../module';
 
-//const resultA = add(2, 3);
-///const resultB = subtract(5, 1);
-//const createdObject = createObject({ a: 'aye', b: 'be' });
+import './home.scss';
 
-//console.log(resultA, resultB);
-//console.log(createdObject);
+const GRID_WIDTH = 3; //32;
+const GRID_HEIGHT = 3; //32;
 
-function Home() {
-  return (
-    <div>Test page</div>
-  );
+class Home extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      grid: [],
+    };
+
+    this.startSimulation = this.startSimulation.bind(this);
+    this.stopSimulation = this.stopSimulation.bind(this);
+    this.runCycle = this.runCycle.bind(this);
+  }
+
+  componentDidMount() {
+    this.randomizeGrid();
+  }
+
+  componentWillUnmount() {
+    this.stopSimulation();
+  }
+
+  getRandomBoolean() {
+    return Math.floor(Math.random() * Math.floor(2)) > 0;
+  }
+
+  randomizeGrid() {
+    const grid = new Array(GRID_HEIGHT);
+    for (let y = 0; y < GRID_HEIGHT; y++) {
+      grid[y] = new Array(GRID_WIDTH);
+      for (let x = 0; x < GRID_WIDTH; x++) {
+        grid[y][x] = this.getRandomBoolean();
+      }
+    }
+
+    this.setState({
+      grid: grid,
+    }, this.startSimulation);
+  }
+
+  startSimulation() {
+    const simulationInterval = setInterval(this.runCycle, 2000);
+    this.setState({
+      simulationInterval: simulationInterval,
+    })
+  }
+
+  stopSimulation() {
+    if (this.state.simulationInterval) {
+      clearInterval(this.state.simulationInterval);
+      this.setState({
+        simulationInterval: null,
+      });
+    }
+  }
+
+  runCycle() {
+    const oldGrid = this.state.grid;
+
+    const newGrid = new Array(GRID_HEIGHT);
+    for (let y = 0; y < GRID_HEIGHT; y++) {
+      newGrid[y] = new Array(GRID_WIDTH);
+      for (let x = 0; x < GRID_WIDTH; x++) {
+        const alive = this.countSurrounding(x, y);
+        if (oldGrid[y, x]) {
+          // current cell is alive
+          if (alive >= 2 && alive <= 3) {
+            newGrid[y][x] = true;
+          } else {
+            newGrid[y][x] = false;
+          }
+        } else {
+          // current cell is dead
+          if (alive === 3) {
+            newGrid[y][x] = true;
+          }
+        }
+      }
+    }
+
+    //console.log('runCycle', newGrid);
+
+    this.setState({
+      grid: newGrid,
+    });
+  }
+
+  countSurrounding(x, y) {
+    let alive = 0;
+
+    // top
+    if (y > 0) {
+      const rowResult = this.countRow(x, y - 1);
+      alive += rowResult;
+    }
+
+    // middle
+    const rowResult = this.countRow(x, y, false);
+    alive += rowResult;
+
+    // bottom
+    if (y < GRID_HEIGHT - 1) {
+      const rowResult = this.countRow(x, y + 1);
+      alive += rowResult;
+    }
+
+    return alive;
+  }
+
+  countRow(x, y, countMiddle = true) {
+    let alive = 0;
+    const grid = this.state.grid;
+    const row = grid[y];
+
+    // left
+    if (x > 0) {
+      if (row[x - 1]) {
+        alive++;
+      }
+    }
+
+    // center
+    if (countMiddle) {
+      if (row[x]) {
+        alive++;
+      }
+    }
+
+    // right
+    if (x < GRID_WIDTH - 1) {
+      if (row[x + 1]) {
+        alive++;
+      }
+    }
+
+    return alive;
+  }
+
+  render() {
+    const grid = this.state.grid;
+
+    return (
+      <div className="main">
+        <div className="toolbar">
+          { this.state.simulationInterval
+            ? <button onClick={this.stopSimulation}>Stop</button>
+            : <button onClick={this.startSimulation}>Start</button>
+          }
+        </div>
+        <div className="grid-of-life">
+          { grid.map((row, rowIndex) => {
+            return (
+              <div key={rowIndex} className="grid-row">
+                { row.map((cell, cellIndex) => {
+                  return (
+                    <div key={cellIndex} className={'grid-cell ' + (cell ? 'alive' : 'dead')}>
+                    </div>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
